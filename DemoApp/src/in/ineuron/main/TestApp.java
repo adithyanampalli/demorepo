e5c8701cc71a05ea42ecfd88b9f5c53b83644d88
Adithya <adithyanampalli@gmail.com>
2025-09-07 22:04:26 +0530

    first commit

---
+++ DemoApp/src/in/ineuron/main/TestApp.java
@@ -0,0 +1,12 @@
+package in.ineuron.main;
+
+public class TestApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("helo gm");
+		System.out.println("jii");
+
+	}
+
+}
